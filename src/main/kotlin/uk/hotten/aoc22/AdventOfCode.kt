e77067303d71882ab772f9048d897c1b8d08485a
IsGabriellaCurious <gabriellakaitlyn@outlook.com>
2022-12-05 10:51:23 +0000

    Day five!

--- src/main/kotlin/uk/hotten/aoc22/AdventOfCode.kt
+++ src/main/kotlin/uk/hotten/aoc22/AdventOfCode.kt
@@ -1,9 +1,9 @@
 package uk.hotten.aoc22;
 
-import uk.hotten.aoc22.days.Day4
+import uk.hotten.aoc22.days.Day5
 
 fun main() {
-    val day = Day4()
+    val day = Day5()
 
     day.part1()
     day.part2()
